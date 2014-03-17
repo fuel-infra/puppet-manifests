2668a19084b5527c88e01a9369eb2c7d9c25739c
Igor Shishkin <me@teran.ru>
2014-03-17 19:13:50 +0400

    transmission-daemon download directory creation
    Change-Id: Iebcc61a8b06d4df8c3e7b600176c258cba8242c4

--- src/modules/transmission_daemon/manifests/init.pp
+++ src/modules/transmission_daemon/manifests/init.pp
@@ -4,6 +4,8 @@ class transmission_daemon {
   $config = $transmission_daemon::params::config
   $packages = $transmission_daemon::params::packages
   $service = $transmission_daemon::params::service
+  $download_dir = $transmission_daemon::params::download_dir
+  $utp_enabled = $transmission_daemon::params::utp_enabled
 
   package { $packages :
     ensure => 'latest',
@@ -18,6 +20,13 @@ class transmission_daemon {
     content => template('transmission_daemon/settings.json.erb'),
   }
 
+  file { $download_dir :
+    path => $download_dir,
+    ensure => 'directory',
+    owner => 'debian-transmission',
+    group => 'debian-transmission',
+  }
+
   exec { "${service}-reload" :
     command => "service ${service} stop ; cp ${config}-new ${config} ; service ${service} start",
     provider => 'shell',
@@ -27,7 +36,8 @@ class transmission_daemon {
 
   Class['dpkg'] ->
     Package[$packages] ->
-    File["${config}-new"] ~>
+    File["${config}-new"] ->
+    File[$download_dir] ~>
     Exec["${service}-reload"]
 
   File["${config}-new"] ~>
--- src/modules/transmission_daemon/manifests/params.pp
+++ src/modules/transmission_daemon/manifests/params.pp
@@ -1,7 +1,9 @@
 class transmission_daemon::params {
   $config = '/etc/transmission-daemon/settings.json'
+  $download_dir = '/srv/downloads'
   $packages = [
     'transmission-daemon'
   ]
   $service = 'transmission-daemon'
+  $utp_enabled = false
 }
