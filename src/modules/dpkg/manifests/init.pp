6908455dce6301d742b38e67a88592432df3d03d
Igor Shishkin <me@teran.ru>
2014-04-28 16:54:32 +0400

    Local mirrors usage
    Change-Id: I06ba7a8fc0f63e7443a751b9639d1668263090dc

--- src/modules/dpkg/manifests/init.pp
+++ src/modules/dpkg/manifests/init.pp
@@ -3,6 +3,16 @@ class dpkg {
 
   $gpg_key_cmd = $dpkg::params::gpg_key_cmd
   $init_command = $dpkg::params::init_command
+  $mirror = $dpkg::params::mirror
+  $repo_list = $dpkg::params::repo_list
+
+  file { $repo_list :
+    path => $repo_list,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => template('dpkg/sources.list.erb'),
+  }
 
   exec { $gpg_key_cmd :
     command => $gpg_key_cmd,
@@ -21,6 +31,7 @@ class dpkg {
   }
 
   Exec[$gpg_key_cmd]->
+    File[$repo_list]->
     Exec[$init_command]
 }
 
--- src/modules/dpkg/manifests/params.pp
+++ src/modules/dpkg/manifests/params.pp
@@ -1,4 +1,24 @@
 class dpkg::params {
   $gpg_key_cmd = 'cat /etc/puppet/modules/dpkg/files/qa-ubuntu.key | apt-key add -'
+
   $init_command = 'apt-get update'
+
+  if $::fqdn =~ /msk\.mirantis\.net$/ {
+    # Moscow internal mirror
+    $mirror = 'mirrors.msk.mirantis.net'
+  }
+  elsif $::fqdn =~ /srt\.mirantis\.net$/ {
+    # Saratov internal mirror
+    $mirror = 'mirrors.srt.mirantis.net'
+  }
+  elsif $::fqdn =~ /\.vm\.mirantis\.net$/ {
+    # VMs, I home they're in Moscow ;)
+    $mirror = 'mirrors.msk.mirantis.net'
+  } else {
+    # All other servers
+    $mirror = 'archive.ubuntu.com'
+  }
+
+  $repo_list = '/etc/apt/sources.list'
 }
+
