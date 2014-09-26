4fb8fe612d75c6278a570b3c115b0266fabae9bc
Igor Shishkin <ishishkin@mirantis.com>
2014-09-26 18:30:03 +0400

    erb template validation added
    Change-Id: I4482f3378914a76c4241a3e82ae5e328ee29dc44

--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -16,3 +16,7 @@ find . -name '*.pp' | xargs -P1 -L1 puppet-lint \
           --no-nested_classes_or_defines-check \
           --no-autoloader_layout-check \
           --no-class_inherits_from_params_class-check
+
+find . -name '*.erb' | xargs -P1 -L1 -I '%' erb \
+          -P -x -T '-' % | ruby -c
+
