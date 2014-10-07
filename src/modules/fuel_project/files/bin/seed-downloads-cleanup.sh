9e80daec5d124317874c00981dbf96061538727b
Igor Shishkin <ishishkin@mirantis.com>
2014-10-07 13:05:38 +0400

    Fix for fuelweb-iso accidentially deletion
    Change-Id: Ie8bdd50166402ef8c0c4ad4e11e20f9f3e0d0558

--- src/modules/fuel_project/files/bin/seed-downloads-cleanup.sh
+++ src/modules/fuel_project/files/bin/seed-downloads-cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/find /srv/downloads /var/www/fuelweb-iso -mtime +10 -delete 2>&1 | logger -t seed-downloads-cleanup
+/usr/bin/find /srv/downloads /var/www/fuelweb-iso -type f -mtime +10 -delete 2>&1 | logger -t seed-downloads-cleanup
 /usr/bin/seedcleaner.py 2>&1 | logger -t seed-downloads-cleanup
