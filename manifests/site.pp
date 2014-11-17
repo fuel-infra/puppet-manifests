3e1267c12a302615c607324bdebc5bfdfe8f89ab
Alexander Charykov <acharykov@mirantis.com>
2014-11-17 19:58:23 +0300

    Fix hostnames for new jenkins slaves
    Change-Id: Ic258ef06902ca9a7049d9cd8fac8f20258e141b7

--- manifests/site.pp
+++ manifests/site.pp
@@ -35,7 +35,7 @@ node /(mc([0-9]+)n([0-9]+)|srv([0-9]+))-(msk|srt)\.(msk|srt)\.mirantis\.net/ {
   }
 }
 
-node /srv(22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37)\.bud\.mirantis\.net/ {
+node /srv(22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37)-bud\.bud\.mirantis\.net/ {
   class { '::fuel_project::jenkins::slave' :
     run_tests      => true,
     build_fuel_iso => true,
