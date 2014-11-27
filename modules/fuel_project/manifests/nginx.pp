0b0ba0b4d7729ea49314465529348dde8e218bee
Igor Shishkin <ishishkin@mirantis.com>
2014-11-27 20:15:00 +0300

    Fix for nginx stub status vhost
    Change-Id: I25197cf8125665906d7bdb0c4bc04f8301d80e0a

--- modules/fuel_project/manifests/nginx.pp
+++ modules/fuel_project/manifests/nginx.pp
@@ -7,7 +7,8 @@ class fuel_project::nginx {
 
   ::nginx::resource::vhost { 'stub_status' :
     ensure              => 'present',
-    listen_port         => '127.0.0.1:61929',
+    listen_ip           => '127.0.0.1',
+    listen_port         => 61929,
     location_custom_cfg => {
       stub_status => true,
     },
