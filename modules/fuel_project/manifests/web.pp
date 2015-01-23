dbb4422246d3e15a1a8c7d413bbbc53a5e7b3d55
Sergey Kulanov <skulanov@mirantis.com>
2015-01-23 16:03:46 +0200

    Move nginx zabbix monitoring staff to nginx stub_status
    We can monitor only if we have stub_status location implemented
    in nginx configuration. For nginx monitoring, just include
    fuel_project::nginx class
    
    Change-Id: I29d79eb15cc6168a2b745c2c395848b12ab90987

--- modules/fuel_project/manifests/web.pp
+++ modules/fuel_project/manifests/web.pp
@@ -4,8 +4,4 @@ class fuel_project::web {
   class { '::fuel_project::nginx' :}
   class { '::fuel_project::common' :}
   class { '::fuel_project::landing_page' :}
-
-  zabbix::item { 'nginx' :
-    content => 'puppet:///modules/fuel_project/web/zabbix/nginx_items.conf',
-  }
 }
