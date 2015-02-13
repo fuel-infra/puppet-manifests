21e6c4f5711f5df4379a51b1f1e1172c7093e241
Eugene Korekin <ekorekin@mirantis.com>
2015-02-13 17:56:26 +0000

    Enabled autofs user homes on TPI hosts
    Change-Id: I0efbb254d24e1cfdb238883ab090d839b76fd16a

--- modules/fuel_project/manifests/tpi/lab.pp
+++ modules/fuel_project/manifests/tpi/lab.pp
@@ -2,6 +2,7 @@
 class fuel_project::tpi::lab (
   $btsync_secret = $fuel_project::tpi::params::btsync_secret,
   $sudo_commands = [ '/sbin/ebtables', '/sbin/iptables' ],
+  $nfs_servers = [ 'tpi-s1', 'tpi-s2' ],
 ) {
 
   class { '::fuel_project::jenkins::slave' :
@@ -18,6 +19,7 @@ class fuel_project::tpi::lab (
     'linux-headers-3.13.0-39-generic',
     'btsync',
     'sudo-ldap',
+    'autofs',
   ]
 
   ensure_packages($tpi_packages)
@@ -61,6 +63,55 @@ class fuel_project::tpi::lab (
     ensure  => 'running',
     enable  => true,
     require => File['/etc/init.d/disable-hugepage-defrag'],
+  }
+
+  #automount
+  file { '/usr/local/jenkins':
+    ensure => 'directory',
+    mode   => '0755',
+    owner  => 'jenkins',
+    group  => 'jenkins',
+  }
+
+  mount { '/usr/local/jenkins':
+    ensure  => 'mounted',
+    device  => '/home/jenkins',
+    fstype  => 'none',
+    options => 'rw,bind',
+    atboot  => true,
+    require => File['/usr/local/jenkins'],
+  }
+
+  file { '/etc/auto.master.d':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'root',
+    group   => 'root',
+    require => Mount['/usr/local/jenkins'],
+  }
+
+  file { '/etc/auto.master.d/home.autofs':
+    ensure  => 'present',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('fuel_project/tpi/home.autofs.erb'),
+    require => File['/etc/auto.master.d']
+  }
+
+  file { '/etc/auto.home':
+    ensure  => 'present',
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('fuel_project/tpi/auto.home.erb'),
+    require => File['/etc/auto.master.d/home.autofs'],
+    notify  => Service['autofs'],
+  }
+
+  service{ 'autofs':
+    ensure => 'running',
+    enable => true
   }
 
 }
