9b95e8b26825d055f2e507d8d6ae03f2f45b84b4
Daniel Shafer <dshafer@mirantis.com>
2015-02-27 14:30:49 -0600

    Add config for US jenkin product slaves
    Added configuration to puppet manifest for 2
    nodes that need added to the Jenkins product
    master so that we may run local tests in the US.
    
    Change-Id: I2c1f9391754d2b5477666b2b8fba3467b6cc4b84
    Partial-bug: #1426480

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,15 @@ node /srv(22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37)-bud\.bud\.mirantis\.n
   }
 }
 
+node /devops-(01|02)\.mnv\.mirantis\.net/ {
+  class { '::fuel_project::jenkins::slave' :
+    run_tests           => true,
+    build_fuel_iso      => true,
+    fuelweb_iso         => true,
+    ldap                => true,
+    jenkins_swarm_slave => true,
+  }
+}
 
 node /cacher01-(cz|kha|mnv|poz)\.vm\.mirantis\.net/ {
   class { '::fuel_project::jenkins::slave' :}
