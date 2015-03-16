b64f0204e7234d2f968c783bd6786372bc10783d
Igor Shishkin <ishishkin@mirantis.com>
2015-03-16 13:05:00 +0300

    Check and fix for hostname if it's properly set added
    Change-Id: I7920b799091effa0e0ad773a00d3a7519f0293fe
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- modules/fuel_project/manifests/common.pp
+++ modules/fuel_project/manifests/common.pp
@@ -24,6 +24,9 @@ class fuel_project::common (
   class { '::ssh::sshd' :
     apply_firewall_rules => $external_host,
   }
+  # TODO: remove ::system module
+  # ... by spliting it's functions to separate modules
+  # or reusing publically available ones
   class { '::system' :}
   class { '::zabbix::agent' :
     apply_firewall_rules => $external_host,
@@ -75,11 +78,16 @@ class fuel_project::common (
     template => 'fuel_project/common/zabbix/software.conf.erb',
   }
 
+  # Zabbix hardware item
   ensure_packages(['smartmontools'])
+
   ::zabbix::item { 'hardware-zabbix-check' :
     content => 'puppet:///modules/fuel_project/common/zabbix/hardware.conf',
+    require => Package['smartmontools'],
   }
+  # /Zabbix hardware item
 
+  # Zabbix SSL item
   file { '/usr/local/bin/zabbix_check_certificate.sh' :
     ensure => 'present',
     mode   => '0755',
@@ -87,10 +95,35 @@ class fuel_project::common (
   }
   ::zabbix::item { 'ssl-certificate-check' :
     content => 'puppet:///modules/fuel_project/common/zabbix/ssl-certificate-check.conf',
+    require => File['/usr/local/bin/zabbix_check_certificate.sh'],
   }
+  # /Zabbix SSL item
 
   mount { '/' :
     ensure  => 'present',
     options => 'defaults,errors=remount-ro,noatime,nodiratime,barrier=0',
+  }
+
+  file { '/etc/hostname' :
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => $::fqdn,
+    notify  => Exec['/bin/hostname -F /etc/hostname'],
+  }
+
+  file { '/etc/hosts' :
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('fuel_project/common/hosts.erb'),
+  }
+
+  exec { '/bin/hostname -F /etc/hostname' :
+    subscribe   => File['/etc/hostname'],
+    refreshonly => true,
+    require     => File['/etc/hostname'],
   }
 }
