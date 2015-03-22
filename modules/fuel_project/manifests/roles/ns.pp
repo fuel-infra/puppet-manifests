7a682eeb8fb2ea24a6131b28c5df03a2208119fb
Igor Shishkin <ishishkin@mirantis.com>
2015-03-22 04:19:34 +0300

    Bind monitoring items added
    Change-Id: I2c3acd651d1fc7f934c641d58d75c4ead6b1de3c
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- modules/fuel_project/manifests/roles/ns.pp
+++ modules/fuel_project/manifests/roles/ns.pp
@@ -38,6 +38,31 @@ class fuel_project::roles::ns (
     }
   }
 
+  ensure_packages(['perl', 'perl-base'])
+
+  file { '/usr/local/bin/bind96-stats-parse.pl' :
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/fuel_project/ns/bind96-stats-parse.pl',
+    require => [
+      Package['perl'],
+      Package['perl-base']
+    ],
+  }
+
+  cron { 'rndc-stats' :
+    command => '>/var/lib/bind/statistics.txt ; /usr/sbin/rndc stats',
+    user    => 'root',
+    minute  => '*/5',
+    require => File['/usr/local/bin/bind96-stats-parse.pl'],
+  }
+
+  ::zabbix::item { 'bind' :
+    content => 'puppet:///modules/fuel_project/ns/zabbix_bind.conf',
+  }
+
   if ($dns_checkout_private_key_content) {
     file { '/root/.ssh' :
       ensure => 'directory',
