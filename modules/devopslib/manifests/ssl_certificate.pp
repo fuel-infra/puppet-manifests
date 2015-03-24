098b9957ec6415012e6def4e782aedc9760d9a66
Alexander Charykov <acharykov@mirantis.com>
2015-03-24 18:10:32 +0300

    Define function for defining ssl certificates from hiera
      Usage: devops::ssl_certificate { 'cert_name' : }
    
    Change-Id: I9ed13fd1ae0f86bdba5de4509cae3f228fc17f3e

---
+++ modules/devopslib/manifests/ssl_certificate.pp
@@ -0,0 +1,43 @@
+# get cerificate from hiera and create then on destination system
+#
+# Hiera usage:
+# ---
+# ssl_certificates::sample_cert:
+#   crt:
+#     filename: '/etc/ssl/cert.crt'
+#     content: |
+#       -----BEGIN CERTIFICATE-----
+#       ...
+#       -----END CERTIFICATE-----
+#   key:
+#     filename: '/etc/ssl/cert.key'
+#     content: |
+#       -----BEGIN RSA PRIVATE KEY-----
+#       ...
+#       -----END RSA PRIVATE KEY-----
+#
+# In puppet manifests:
+# devopslib::ssl_certificate { 'sample_cert':
+#   owner => 'www-data'
+# }
+define devopslib::ssl_certificate(
+  $certificate = hiera_hash("ssl_certificates::${title}"),
+  $owner       = 'root',
+  $group       = 'root',
+  $mode        = '0400',
+) {
+  file { $certificate['crt']['filename'] :
+    ensure  => present,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => $certificate['crt']['content'],
+  }
+  file { $certificate['key']['filename'] :
+    ensure  => present,
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => $certificate['key']['content'],
+  }
+}
