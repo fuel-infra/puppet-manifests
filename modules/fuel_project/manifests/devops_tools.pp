5903df62f5c7d48ed3797655234535ebbb0b72b6
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2015-04-08 12:33:03 +0200

    Add lpupdatebug to devops_tools.pp manifest
    Change-Id: I7779ec19524d14f1ff84ab58a388189e407a736b
    Partial-Bug: #1419246

--- modules/fuel_project/manifests/devops_tools.pp
+++ modules/fuel_project/manifests/devops_tools.pp
@@ -3,4 +3,5 @@
 class fuel_project::devops_tools {
   class { '::fuel_project::common' :}
   class { '::fuel_project::devops_tools::lpbugmanage' :}
+  class { '::fuel_project::devops_tools::lpupdatebug' :}
 }
