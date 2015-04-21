3d0c069f7772ddfd8f640cce21de73b070d223fa
Igor Shishkin <ishishkin@mirantis.com>
2015-04-21 17:37:29 +0300

    puppet master role added and corresponding fact for set up
    Change-Id: I91eacfa9b4e5fc1258e8149f6b4e8c134d8cea87
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- bin/install_puppet_master.sh
+++ bin/install_puppet_master.sh
@@ -10,6 +10,6 @@ apt-get install -y git puppet
 
 /etc/puppet/bin/install_modules.sh
 
-FACTER_PUPPET_APPLY=true puppet apply -vd /etc/puppet/manifests/site.pp
+FACTER_PUPPET_APPLY=true FACTER_ROLE=puppetmaster puppet apply -vd /etc/puppet/manifests/site.pp
 puppet agent --enable
 puppet agent -vd --no-daemonize --onetime
