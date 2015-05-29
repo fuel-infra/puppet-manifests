6ae3f4a86fdd97d7a04740495a073207f5f55b53
Igor Shishkin <ishishkin@mirantis.com>
2015-05-29 16:36:24 +0300

    Splitting errata hosts and starting to use hiera
    Change-Id: If9d10df5994ef3b94ba49b0dea865facb9140058
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -61,8 +61,8 @@ node /docs-slave01.vm.mirantis.net/ {
   class { '::fuel_project::jenkins::slave' :}
 }
 
-node /errata([0-9]+)(\-tst)?\.infra\.mirantis\.net/ {
-  class { '::fuel_project::roles::errata' :}
+node /errata(db)?([0-9]+)(\-tst)?\.(infra\.mirantis\.net|fuel-infra\.org)/ {
+  hiera_include('classes')
 }
 
 node /srv(22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37)-bud\.bud\.mirantis\.net/ {
