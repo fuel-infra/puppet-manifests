d2624b4570af5b4e9d47718ee793f53d8c179ed1
Igor Shishkin <ishishkin@mirantis.com>
2015-06-01 14:18:06 +0300

    Google counter ability to use added
    Change-Id: I186f7e3540786823b9a5f6d63e1cec0d9288f8a3
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- modules/landing_page/manifests/params.pp
+++ modules/landing_page/manifests/params.pp
@@ -2,48 +2,49 @@
 #
 class landing_page::params {
   # Landing page webapp
-  $app_user               = 'release'
-  $apps                   = [
+  $app_user                     = 'release'
+  $apps                         = [
     'landing_page',
     'plugins_catalog',
     'security',
     'sheet'
   ]
-  $apply_firewall_rules   = false
-  $config                 = '/etc/landing_page/settings.py'
-  $config_template        = 'landing_page/landing_page.py.erb'
-  $database_engine        = 'django.db.backends.mysql'
-  $database_host          = 'localhost'
-  $database_name          = 'release'
-  $database_password      = 'release'
-  $database_port          = ''
-  $database_user          = 'release'
-  $debug                  = false
-  $firewall_allow_sources = {}
-  $nginx_access_log       = '/var/log/nginx/access.log'
-  $nginx_error_log        = '/var/log/nginx/error.log'
-  $nginx_log_format       = undef
-  $nginx_server_aliases   = undef
-  $nginx_server_name      = $::fqdn
-  $package                = [
+  $apply_firewall_rules         = false
+  $config                       = '/etc/landing_page/settings.py'
+  $config_template              = 'landing_page/landing_page.py.erb'
+  $database_engine              = 'django.db.backends.mysql'
+  $database_host                = 'localhost'
+  $database_name                = 'release'
+  $database_password            = 'release'
+  $database_port                = ''
+  $database_user                = 'release'
+  $debug                        = false
+  $firewall_allow_sources       = {}
+  $google_analytics_property_id = undef
+  $nginx_access_log             = '/var/log/nginx/access.log'
+  $nginx_error_log              = '/var/log/nginx/error.log'
+  $nginx_log_format             = undef
+  $nginx_server_aliases         = []
+  $nginx_server_name            = $::fqdn
+  $package                      = [
     'landing-page-all',
     'python-mysqldb',
   ]
-  $package_updater        = [
+  $package_updater              = [
     'landing-page-release-status-app-cli',
   ]
-  $plugins_repository     = 'http://127.0.0.1'
-  $ssl                    = true
-  $ssl_cert_file          = '/etc/ssl/release.crt'
-  $ssl_cert_file_contents = undef
-  $ssl_key_file           = '/etc/ssl/release.pem'
-  $ssl_key_file_contents  = undef
-  $timezone               = 'UTC'
-  $uwsgi_socket           = '127.0.0.1:7939'
+  $plugins_repository           = 'http://127.0.0.1'
+  $ssl                          = true
+  $ssl_cert_file                = '/etc/ssl/release.crt'
+  $ssl_cert_file_contents       = undef
+  $ssl_key_file                 = '/etc/ssl/release.pem'
+  $ssl_key_file_contents        = undef
+  $timezone                     = 'UTC'
+  $uwsgi_socket                 = '127.0.0.1:7939'
 
   # Updater app
-  $updater_app            = 'release'
-  $updater_config         = '/etc/release-updater.yaml'
-  $updater_token          = '<SECRET_TOKEN>'
-  $updater_user           = 'jenkins'
+  $updater_app                  = 'release'
+  $updater_config               = '/etc/release-updater.yaml'
+  $updater_token                = '<SECRET_TOKEN>'
+  $updater_user                 = 'jenkins'
 }
