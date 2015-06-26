bb079874a7ef106789ed7570c9c5b91dbc80b621
alexz <azvyagintsev@mirantis.com>
2015-06-26 12:58:28 +0300

    Add default hiera.yaml config
    Default Ubuntu ruby-hiera which use like dependence
    for puppet-master doesn't contain default hiera.yaml config
    
    Change-Id: I7ecaced7404c0456064e4d93de5c7cb6b683a228

--- bin/install_puppet_master.sh
+++ bin/install_puppet_master.sh
@@ -10,6 +10,17 @@ apt-get install -y git puppet apt-transport-https
 
 /etc/puppet/bin/install_modules.sh
 
+expect_hiera=$(puppet apply -vd --genconfig | awk '/ hiera_config / {print $3}')
+if [ ! -f ${expect_hiera} ]; then
+    echo "File ${expect_hiera} not found!"
+    if [ ! -f /etc/hiera.yaml ]; then
+        ln -s /etc/puppet/hiera/hiera-stub.yaml ${expect_hiera}
+    else
+        echo "Found default /etc/hiera.yaml"
+        ln -s /etc/hiera.yaml  ${expect_hiera}
+    fi
+fi
+
 FACTER_PUPPET_APPLY=true FACTER_ROLE=puppetmaster puppet apply -vd /etc/puppet/manifests/site.pp
 puppet agent --enable
 puppet agent -vd --no-daemonize --onetime
