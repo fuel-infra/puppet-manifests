e4f796b1af8f5a52bc7e38fe1a0b32ee913dd225
Damian Szeluga <dszeluga@mirantis.com>
2015-07-07 22:24:04 +0200

    Installation of puppet modules in isolated env
    Change-Id: I7309ce37b8dbc0020e408d71fde36b36028e0469

--- bin/install_puppet_master.sh
+++ bin/install_puppet_master.sh
@@ -6,18 +6,28 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y git puppet apt-transport-https
+apt-get install -y git puppet apt-transport-https tar
 
-/etc/puppet/bin/install_modules.sh
+if [ -z "${PUPPET_MODULES_ARCHIVE}" ]; then
+    /etc/puppet/bin/install_modules.sh
+else
+    MODULEPATH=$(puppet config print | awk -F':' '/^modulepath/{print $NF}')
+    if [ -f "${PUPPET_MODULES_ARCHIVE}" ]; then
+        tar xvf "${PUPPET_MODULES_ARCHIVE}" --strip-components=1 -C "${MODULEPATH}"
+    else
+        echo "${PUPPET_MODULES_ARCHIVE} is not a file. Quitting!"
+        exit 2
+    fi
+fi
 
 expect_hiera=$(puppet apply -vd --genconfig | awk '/ hiera_config / {print $3}')
-if [ ! -f ${expect_hiera} ]; then
+if [ ! -f "${expect_hiera}" ]; then
     echo "File ${expect_hiera} not found!"
     if [ ! -f /etc/hiera.yaml ]; then
-        ln -s /etc/puppet/hiera/hiera-stub.yaml ${expect_hiera}
+        ln -s /etc/puppet/hiera/hiera-stub.yaml "${expect_hiera}"
     else
         echo "Found default /etc/hiera.yaml"
-        ln -s /etc/hiera.yaml  ${expect_hiera}
+        ln -s /etc/hiera.yaml  "${expect_hiera}"
     fi
 fi
 
