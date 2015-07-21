7402ddc6b6ef636b6c003ecce247c647dba351a1
Alexander Charykov <acharykov@mirantis.com>
2015-07-21 19:25:46 +0300

    Move keys from parameters to class
    Change-Id: Ib4e396c820f70d94363131fa573ee9800d8cd208

--- modules/ssh/manifests/authorized_keys.pp
+++ modules/ssh/manifests/authorized_keys.pp
@@ -1,7 +1,6 @@
 # Class: ssh::authorized_keys
-class ssh::authorized_keys(
+class ssh::authorized_keys {
   $keys = hiera_hash('ssh::authorized_keys::keys', {})
-) {
   create_resources(ssh_authorized_key,
     $keys, {
       ensure => present,
