f26f1ca5f9e8ebbabdbd340172904b5d94a239cb
Sergey Kulanov <skulanov@mirantis.com>
2015-07-23 15:42:55 +0300

    Refactoring perestroika
    1) Since perestroika needs only ::jenkins::slave we don't need
    ::fuel_project::jenkins::slave class as it's overloaded now
    and should be refactored in future
    
    2) We DO NOT need to clean known_hosts file (overwrite => false),
    since ssh::known_hosts for now adds only the last record by
    design. This should be also refactored
    
    3) Perestroika can be deployed as single application without
    any external classes
    
    Change-Id: I1df506b66ea4d80e5be319572671e5fba2753c33

--- modules/fuel_project/manifests/roles/perestroika/builder.pp
+++ modules/fuel_project/manifests/roles/perestroika/builder.pp
@@ -4,7 +4,10 @@
 # see hiera file for list and params of used classes
 
 class fuel_project::roles::perestroika::builder (
-  $packages = [
+  $docker_package,
+  $builder_user = 'jenkins',
+  $known_hosts  = undef,
+  $packages     = [
     'createrepo',
     'devscripts',
     'git',
@@ -12,8 +15,30 @@ class fuel_project::roles::perestroika::builder (
     'reprepro',
     'yum-utils',
   ],
-) {
+){
 
-  # install packages
+  # ensure build user exists
+  ensure_resource('user', $builder_user, {
+    'ensure' => 'present'
+  })
+
+  # install required packages
   ensure_packages($packages)
+  ensure_packages($docker_package)
+
+  # ensure $builder_user in docker group
+  # docker group will be created by docker package
+  User <| title == $builder_user |> {
+    groups  +> 'docker',
+    require => Package[$docker_package],
+  }
+
+  if ($known_hosts) {
+    create_resources('ssh::known_host', $known_hosts, {
+      user      => $builder_user,
+      overwrite => false,
+      require   => User[$builder_user],
+    })
+  }
+
 }
