a57f7ac0d6a5703615e3be8562cc074c8ab801c8
Igor Shishkin <ishishkin@mirantis.com>
2015-08-05 15:57:39 +0300

    Zabbix maintenance script added.
    To use for automatic Zabbix DB partitioning for now
    
    Change-Id: Ie6b876ef191e5afd5880ec0bca5d12a6272c1bdb
    Signed-off-by: Igor Shishkin <ishishkin@mirantis.com>

--- modules/fuel_project/manifests/roles/zabbix/server.pp
+++ modules/fuel_project/manifests/roles/zabbix/server.pp
@@ -4,6 +4,8 @@ class fuel_project::roles::zabbix::server (
   $mysql_replication_password = '',
   $mysql_replication_user     = 'repl',
   $mysql_slave_host           = undef,
+  $maintenance_script         = '/usr/share/zabbix-server-mysql/maintenance.sh',
+  $maintenance_script_config  = '/root/.my.cnf',
   $server_role                = 'master', # master || slave
   $slack_emoji_ok             = ':smile:',
   $slack_emoji_problem        = ':frowning:',
@@ -36,6 +38,22 @@ class fuel_project::roles::zabbix::server (
       privileges => ['REPLICATION SLAVE'],
       table      => '*.*',
       user       => "${mysql_replication_user}@${mysql_slave_host}",
+    }
+
+    file { $maintenance_script :
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      content => template('fuel_project/roles/zabbix/server/maintenance.sh.erb'),
+      require => Class['zabbix::server'],
+    }
+
+    cron { 'zabbix-maintenance' :
+      ensure  => 'present',
+      command => "${maintenance_script} 2>&1 | logger -t zabbix-maintenance",
+      weekday => 'Wednesday',
+      hour    => '15',
     }
   }
 }
