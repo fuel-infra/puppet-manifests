b664917ef3dee132be46016ea0b4178ad8ca48dd
Artur Kaszuba <akaszuba@mirantis.com>
2015-09-15 16:42:47 +0200

    Add compression for console output from jenkins jobs
    Change-Id: Iab3dbbac1776fe89949092385bee0e477b41a251

--- modules/fuel_project/manifests/jenkins/master.pp
+++ modules/fuel_project/manifests/jenkins/master.pp
@@ -5,6 +5,12 @@ class fuel_project::jenkins::master (
   $install_label_dumper = false,
   $install_plugins      = false,
   $install_zabbix_item  = false,
+  $jobs_dir             = '/var/lib/jenkins/jobs/',
+  $log_gzip_cron_name   = 'jenkins-log-compression',
+  $log_gzip_cron_params = {},
+  $log_gzip_days        = '1',
+  $log_gzip_enable      = false,
+  $log_gzip_threads     = '1',
   $service_fqdn         = $::fqdn,
 ) {
   class { '::fuel_project::common':
@@ -21,5 +27,15 @@ class fuel_project::jenkins::master (
       ensure  => present,
       require => Service['jenkins'],
     }
+  }
+  if($log_gzip_enable) {
+    ensure_packages('pigz')
+    create_resources('cron', { "${log_gzip_cron_name}" => $log_gzip_cron_params }, {
+        ensure  => 'present',
+        command => "/usr/bin/flock -xn /var/lock/${log_gzip_cron_name}.lock /usr/bin/find ${jobs_dir} -name 'log' -regex '.*/builds/[0-9]+/log' -mtime +${log_gzip_days} -exec pigz -p ${log_gzip_threads} '{}' \; 2>&1 | logger -t jenkins-log-compression",
+        user    => 'jenkins',
+        minute  => '30',
+        hour    => '2',
+    })
   }
 }
