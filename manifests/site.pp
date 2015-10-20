703059307206333e60b9ab84475b14840202a72b
Igor Shishkin <me@teran.ru>
2015-10-20 17:49:53 +0300

    ns role migration to hiera
    Change-Id: Iba754a7be13c9a78454d5083537ff5e811046c60
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- manifests/site.pp
+++ manifests/site.pp
@@ -190,10 +190,6 @@ node 'mongo-secondary-2.vm.mirantis.net' {
   }
 }
 
-node /ns([0-9]{2})-(bud|kha|mnv|msk|poz|srt)\.devops\.mirantis\.net/ {
-  class { '::fuel_project::roles::ns' :}
-}
-
 node /storage([0-9]{2})-(msk|srt)\.devops\.mirantis\.net/ {
   class { '::fuel_project::roles::storage' :}
 }
