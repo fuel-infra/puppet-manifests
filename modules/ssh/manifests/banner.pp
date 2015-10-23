a4a41b34c757c5efd693389a55683b375fc6bce3
Igor Shishkin <me@teran.ru>
2015-10-23 17:59:07 +0300

    Motd banner moved to hiera
    Change-Id: Ic464dd66c3cd6d454c2a270dcc6bed0ea5da5bb2
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- modules/ssh/manifests/banner.pp
+++ modules/ssh/manifests/banner.pp
@@ -1,10 +1,12 @@
 # Class: ssh::banner
 #
-class ssh::banner {
+class ssh::banner (
+  $content = '',
+) {
   file { '/etc/banner' :
     owner   => 'root',
     group   => 'root',
     mode    => '0400',
-    content => template('ssh/banners/mirantis.net_ldap.erb')
+    content => $content,
   }
 }
