3253173fca79db52c45ba81ae93ce6e9c035ed4f
Andrey Nikitin <anikitin@mirantis.com>
2015-10-26 14:10:22 +0000

    Added manifests to configure Blackduck's server and client
     * added new puppet manifests to configure Blackduck's
       server and client;
     * added new hiera-roles for Blackduck into hiera/roles:
       - added new role into manifests to configure Blackduck's
         server instance;
       - added new role into manifests to configure Blackduck's
         client instance (with SDK);
      * added a template for file with Google's credentials.
    
    Change-Id: I76572a0aca658ebc2969f53ba6cd3f8c243b3899
    Related Jira-ticket: https://mirantis.jira.com/browse/PROD-2110

---
+++ modules/fuel_project/manifests/roles/blackduck/client.pp
@@ -0,0 +1,109 @@
+# Class: fuel_project::roles::blackduck::server::client
+#
+# This class is about configuration of Blackduck's client.
+# [*google_creds*] - JSON-hash to have an ability to work
+#  with Google services (Google Drive, for example).
+#  Hash in the following form:
+#   type: 'service_account'
+#   private_key_id: 'put_here_your_id'
+#   private_key: 'put_here_your_key'
+#   client_email: 'put_here_your_client_email'
+#   client_id: 'put_here_your_client_id'
+#   auth_uri: 'https://accounts.google.com/o/oauth2/auth'
+#   token_uri: 'https://accounts.google.com/o/oauth2/token'
+#   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs'
+#   client_x509_cert_url: 'put_here_your_client_x509_cert_url'
+#
+#  [*known_hosts*] - set of hosts to add them into known_hosts file.
+#  Set in the following form:
+#   'user@server.test.local':
+#     host: 'user@server.test.local'
+#     port: 22
+#
+#  [*known_hosts_overwrite*] - boolean, if we should overwrite known_hosts file
+#  [*sdk_dst*] - string, path to directory where SDK should be unpacked
+#  [*sdk_path*] - string, path to director where SDK's distr is placed
+#  [*ssh_private_key*] - string, value of private ssh-key
+#  [*ssh_private_key_path*] - string, path to directory to store private ssh-key
+#
+class fuel_project::roles::blackduck::client (
+  $google_creds          = hiera_hash('fuel_project::roles::blackduck::client::google_creds', undef),
+  $known_hosts           = {},
+  $known_hosts_overwrite = false,
+  $sdk_dst               = '/home/jenkins/',
+  $sdk_path              = '/mnt/Export-SDK.zip',
+  $ssh_private_key       = undef,
+  $ssh_private_key_path  = '/home/jenkins/.ssh'
+) {
+
+  $dependencies = [
+    'dpkg-dev',
+    'libblackduck-sampledoanalysis-java',
+    'libblackduck-sampleupdatesourcedirectory-java',
+    'python-grab',
+    'python-gspread',
+    'python-oauth2client',
+    'python-selection',
+    'python-simplejson',
+    'python-user-agent',
+    'reprepro',
+    'rpm',
+    'rsync',
+    'unzip',
+    'weblib',
+  ]
+
+  if ($dependencies) {
+    ensure_packages($dependencies)
+  }
+
+  file { "${ssh_private_key_path}/id_rsa" :
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    mode    => '0400',
+    content => $ssh_private_key,
+    replace => true,
+    require => File[$ssh_private_key_path],
+  }
+
+  file { $sdk_dst:
+    ensure => 'directory',
+  }
+
+  exec { 'unzip_sdk':
+    command => "/usr/bin/unzip ${sdk_path} -d ${sdk_dst}",
+    user    => 'jenkins',
+    require => [
+      Package[$dependencies],
+      File[$sdk_dst],
+      User['jenkins']
+    ],
+  }
+
+  file { '/etc/blackduck' :
+    ensure => 'directory',
+    owner  => 'jenkins',
+    group  => 'jenkins',
+    mode   => '0600',
+  }
+
+  file { '/etc/blackduck/credentials.json':
+    ensure  => 'present',
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    mode    => '0400',
+    content => template('fuel_project/roles/blackduck/credentials.json.erb'),
+    require => [
+      File['/etc/blackduck'],
+    ]
+  }
+
+  if ($known_hosts) {
+    create_resources('::ssh::known_host', $known_hosts, {
+      user      => 'jenkins',
+      overwrite => $known_hosts_overwrite,
+      require   => User['jenkins'],
+    })
+  }
+
+}
---
+++ modules/fuel_project/manifests/roles/blackduck/server.pp
@@ -0,0 +1,130 @@
+# Class: fuel_project::roles::blackduck::server::server
+#
+# This class is about configuration of Blackduck's server.
+#
+# [*blackduck_release*] - string, version of operation system
+# on a goal server.
+#
+# [*database_path*] - string, directory where Blackduck's database
+# is placed. This variable is used to check that database exists.
+#
+# [*iso*] - string, path to the image with Blackduck distro
+#
+# [*mount_point*] - string, path to mount the iso
+#
+# [*ssh_public_key*] - string, value of public ssh-key for an user
+# from Blackduck client
+#
+class fuel_project::roles::blackduck::server (
+  $blackduck_release = 'Red Hat Enterprise Linux Server release 6.2 (Santiago)',
+  $database_path     = '/var/lib/bds-export',
+  $iso               = '/mnt/image.iso',
+  $mount_point       = '/mnt/blackduck_distr',
+  $ssh_public_key    = undef,
+) {
+
+  $dependencies = [
+    'rsync',
+    'rssh',
+    'unzip',
+  ]
+
+  user { 'blackduck':
+    ensure     => 'present',
+    managehome => true,
+    name       => 'blackduck',
+    shell      => '/usr/bin/rssh',
+  }
+
+  class { 'rssh' :
+    allow   => ['rsync'],
+    users   => ['blackduck:022:10000:'],
+    require => [
+      User['blackduck'],
+    ],
+  }
+
+  if ($ssh_public_key) {
+    ssh_authorized_key { 'blackduck' :
+      user    => 'blackduck',
+      type    => 'ssh-rsa',
+      key     => $ssh_public_key,
+      require => [
+        User['blackduck'],
+      ]
+    }
+  }
+
+  ensure_packages($dependencies)
+
+  file { '/etc/blackduck-release':
+    ensure  => 'present',
+    content => $blackduck_release,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  file { $mount_point:
+    ensure  => 'directory',
+  }
+
+  if ($iso) {
+    mount { $mount_point:
+      ensure  => 'mounted',
+      name    => $mount_point,
+      options => 'loop,ro',
+      fstype  => 'iso9660',
+      device  => $iso,
+      require => [
+        File[$mount_point],
+      ]
+    }
+
+  }
+
+  else {
+    fail()
+  }
+
+  exec { 'install_server':
+    command  => './install.sh -i silent',
+    cwd      => $mount_point,
+    provider => shell,
+    timeout  => 600,
+    returns  => [0, 255],
+    unless   => 'bash -c "test -d /var/lib/bds-export"',
+    onlyif   => [
+      "test -f ${mount_point}/install.sh",
+    ],
+    require  => [
+      Mount[$mount_point],
+      Package[$dependencies],
+    ]
+  }
+
+  service { 'bds-exportIP-postgresql':
+    ensure   => 'running',
+    enable   => true,
+    provider => 'base',
+    require  => [
+      Exec['install_server'],
+    ],
+    start    => '/etc/init.d/bds-exportIP-postgresql start',
+    status   => '/etc/init.d/bds-exportIP-postgresql status',
+    stop     => '/etc/init.d/bds-exportIP-postgresql stop',
+  }
+
+  service { 'bds-exportIP-tomcat':
+    ensure   => 'running',
+    enable   => true,
+    provider => 'base',
+    require  => [
+      Exec['install_server'],
+      Service['bds-exportIP-postgresql'],
+    ],
+    start    => '/etc/init.d/bds-exportIP-tomcat start',
+    status   => '/etc/init.d/bds-exportIP-tomcat status',
+    stop     => '/etc/init.d/bds-exportIP-tomcat stop',
+  }
+}
