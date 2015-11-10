7c34a7f034781272d20d4ff1a17fef429dde9a03
Igor Shishkin <me@teran.ru>
2015-11-10 15:48:22 +0300

    ssh_authorized_key cleanup before add behaviour implementation
    Yeah, I know it's ughly, but purge_ssh_keys => true appeared in 3.7.x only :(
    
    Change-Id: Ia13db6c6f6bb3f542f68160b77585b19c377e1ce
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- modules/ssh/manifests/authorized_keys.pp
+++ modules/ssh/manifests/authorized_keys.pp
@@ -1,10 +1,21 @@
 # Class: ssh::authorized_keys
 class ssh::authorized_keys {
   $keys = hiera_hash('ssh::authorized_keys::keys', {})
+  # FIXME: It's ugly dirty hack to do purge_ssh_key with puppet 3.4.x :(
+  # We could replace after upgrade to 3.7+ with:
+  # user { 'root' :
+  #   purge_ssh_key => true,
+  # }
+  #
+  exec { 'echo "" > /root/.ssh/authorized_keys' :
+    user => 'root',
+  }
+  # /FIXME
   create_resources(ssh_authorized_key,
     $keys, {
-      ensure => present,
-      user => 'root'
+      ensure  => present,
+      user    => 'root',
+      require => Exec['echo "" > /root/.ssh/authorized_keys'],
     }
   )
 }
