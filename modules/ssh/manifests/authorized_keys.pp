9c76d5e084cb66464239e438d1947a97bc7647b4
Igor Shishkin <me@teran.ru>
2015-11-20 13:49:46 +0300

    Revert "ssh_authorized_key cleanup before add behaviour implementation"
    This reverts commits:
      7c34a7f034781272d20d4ff1a17fef429dde9a03
      8cd0d7557fa3fa2e10352fb17d40851b07fd020e
    
    Change-Id: I8009699dde66e4fe09f168efa2b9bb2657b2ed55

--- modules/ssh/manifests/authorized_keys.pp
+++ modules/ssh/manifests/authorized_keys.pp
@@ -1,28 +1,10 @@
 # Class: ssh::authorized_keys
 class ssh::authorized_keys {
   $keys = hiera_hash('ssh::authorized_keys::keys', {})
-  # FIXME: It's ugly dirty hack to do purge_ssh_key with puppet 3.4.x :(
-  # We could replace after upgrade to 3.7+ with:
-  # user { 'root' :
-  #   purge_ssh_key => true,
-  # }
-  #
-  file { '/root/.ssh' :
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0700',
-  }
-  exec { 'echo "" > /root/.ssh/authorized_keys' :
-    user    => 'root',
-    require => File['/root/.ssh'],
-  }
-  # /FIXME
   create_resources(ssh_authorized_key,
     $keys, {
-      ensure  => present,
-      user    => 'root',
-      require => Exec['echo "" > /root/.ssh/authorized_keys'],
+      ensure => present,
+      user => 'root'
     }
   )
 }
