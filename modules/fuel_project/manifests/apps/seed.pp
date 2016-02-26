71bb86ed9c459c5a5cdc32d787f83f9e1cda36c7
Igor Shishkin <me@teran.ru>
2016-02-26 19:58:13 +0300

    Refactoring for seeds creation
    Now it allows to set up vhosts for shares via hiera,
    without any additional classes creation which is required
    to speed up such operations and remove any legacy and "custom"
    configurations for really similar entities.
    
    Change-Id: Id838af3a99d8066924c2bbba45727eb6897cc064
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- modules/fuel_project/manifests/apps/seed.pp
+++ modules/fuel_project/manifests/apps/seed.pp
@@ -4,80 +4,75 @@
 # images from a different locations.
 #
 # Parameters:
-#   [*apply_firewall_rules*] - add firewall rules for declared for this class
-#   [*client_max_body_size*] - maximum size of single file uploaded by PUT
-#   [*nginx_access_log*] - access log path
-#   [*nginx_error_log*] - error log path
-#   [*nginx_log_format*] - log format
-#   [*seed_cleanup_dirs*] - directories to clean by downloads_cleaner
-#   [*seed_dir*] - seed storage directory
-#   [*seed_port*] - daemon listening port
-#   [*service_fqdn*] - FQDN for the service
-#   [*vhost_acl_allow*] - hosts allowed to PUT data
+#   [*seed_acl_list*] - Array, list of hosts to be allowed to connect to nginx
+#     upload vhost.
+#   [*shares*] - Hash, Describes the share in following form:
+#     $shares =  {
+#       'mylovelyshare' => {
+#         'service_fqdn'        => 'myhost.example.com',
+#         'path'                => '/var/www/myshare',
+#         'autoindex'           => false,
+#         'http_ro'             => true,
+#         'ssh_authorized_keys' => {
+#           'mykey' => {
+#             'type': 'ssh-rsa',
+#             'key': '<SSH PUBLIC KEY>'
+#           }
+#         }
+#       }
+#     }
+#
+#     For more parameters please refer to ::fuel_project::apps::share
+#
+#   [*cleanup_dirs*] - Array, list of hashes contains dirs to clean up
+#     pereodically in the following form:
+#     $cleanup_dirs = [
+#       {
+#         'dir'     => '/var/www/dir',
+#         'ttl'     => '10' # in days
+#         'pattern' => '*.txt'
+#       }
+#     ]
 #
 class fuel_project::apps::seed (
-  $apply_firewall_rules = false,
-  $client_max_body_size = '5G',
-  $nginx_access_log     = '/var/log/nginx/access.log',
-  $nginx_error_log      = '/var/log/nginx/error.log',
-  $nginx_log_format     = 'proxy',
-  $seed_cleanup_dirs    = undef,
-  $seed_dir             = '/var/www/seed',
-  $seed_port            = 17333,
-  $service_fqdn         = "seed.${::fqdn}",
-  # FIXME: Make one list for hosts on L3 and L7
-  $vhost_acl_allow = [],
+  $seed_acl_list,
+  $shares = {},
+  $cleanup_dirs = [],
 ) {
-  if (!defined(Class['::fuel_project::nginx'])) {
-    class { '::fuel_project::nginx' :}
-  }
-  ::nginx::resource::vhost { 'seed' :
-    ensure           => 'present',
-    access_log       => $nginx_access_log,
-    error_log        => $nginx_error_log,
-    format_log       => $nginx_log_format,
-    www_root         => $seed_dir,
-    server_name      => [$service_fqdn, $::fqdn],
-    vhost_cfg_append => {
-      autoindex        => 'off',
-      disable_symlinks => 'if_not_owner',
-    },
-  }
-
-  ::nginx::resource::vhost { 'seed-upload' :
-    ensure              => 'present',
-    autoindex           => 'off',
-    www_root            => $seed_dir,
-    listen_port         => $seed_port,
-    server_name         => [$::fqdn],
-    access_log          => $nginx_access_log,
-    error_log           => $nginx_error_log,
-    format_log          => $nginx_log_format,
-    location_cfg_append => {
-      dav_methods          => 'PUT',
-      client_max_body_size => $client_max_body_size,
-      allow                => $vhost_acl_allow,
-      deny                 => 'all',
-      disable_symlinks     => 'if_not_owner',
-    }
-  }
-
   ensure_resource('file', '/var/www', {
     ensure => 'directory',
     owner => 'root',
     group => 'root',
     mode => '0755',
-    before => File[$seed_dir],
   })
 
-  file { $seed_dir :
-    ensure  => 'directory',
-    owner   => 'www-data',
-    group   => 'www-data',
-    require => Class['nginx'],
+  class {'::devopslib::downloads_cleaner' :
+    cleanup_dirs => $cleanup_dirs,
   }
 
-  class {'::devopslib::downloads_cleaner' :
-    cleanup_dirs => $seed_cleanup_dirs,
+  create_resources('::fuel_project::apps::share', $shares, {})
+
+  # FIXME {
+  # Backward compability for uploading artefacts via HTTP
+  # Should be refactored using rsync+ssh
+  #
+  ::nginx::resource::vhost { 'seed-upload' :
+    ensure              => 'present',
+    autoindex           => 'off',
+    www_root            => '/var/www/seed',
+    listen_port         => '17333',
+    server_name         => [$::fqdn],
+    access_log          => '/var/log/nginx/access.log',
+    error_log           => '/var/log/nginx/error.log',
+    format_log          => 'proxy',
+    location_cfg_append => {
+      dav_methods          => 'PUT',
+      client_max_body_size => '5G',
+      allow                => $seed_acl_list,
+      deny                 => 'all',
+      disable_symlinks     => 'if_not_owner',
+    },
+    require             => Class['fuel_project::nginx'],
   }
+  # } FIXME
 }
---
+++ modules/fuel_project/manifests/apps/share.pp
@@ -0,0 +1,145 @@
+#
+# Define: fuel_project::apps::share
+#
+# Generic definition to create web shares
+#
+# Parameters:
+#   [*service_fqdn*] - String, FQDN for the service, will be used as nginx's
+#     server_name
+#   [*path*] - String, local directory path to serve
+#   [*autoindex*] - Boolean, Enables or disables nginx's autoindex operation
+#   [*http_ro*] - Boolean, Enables or disables nginx configuration
+#   [*nginx_access_log*] - String, nginx access log path
+#   [*nginx_error_log*] - String, nginx error log path
+#   [*nginx_log_format*] - String, nginx log format name
+#   [*sync_hosts_allow*] - Array, list of hosts to allow rsync connections from
+#     if $rsync_rw is enabled
+#   [*rsync_ro*] - Boolean, Enables or disables rsync read-only module
+#   [*rsync_rw*] - Boolean, Enables or disables rsync writable module
+#   [*ssh_authorized_keys*] - Hash, Hash of keys(to pass to create_resources()
+#     function)
+#   [*username*] - String, System user name to use as share owner
+#   [*htpasswd*] - String, Enables or disables Basic auth nginx's feature.
+#     If not empty should contain htpasswd file content
+#
+define fuel_project::apps::share (
+  $service_fqdn,
+  $path,
+  $autoindex           = false,
+  $http_ro             = true,
+  $nginx_access_log    = '/var/log/nginx/access.log',
+  $nginx_error_log     = '/var/log/nginx/error.log',
+  $nginx_log_format    = 'proxy',
+  $sync_hosts_allow    = [],
+  $rsync_ro            = false,
+  $rsync_rw            = false,
+  $ssh_authorized_keys = {},
+  $username            = $title,
+  $htpasswd            = '',
+) {
+  include ::rssh
+
+  user { $username :
+    ensure     => 'present',
+    home       => $path,
+    managehome => true,
+    shell      => '/usr/bin/rssh',
+    system     => true,
+  }
+
+  file { $path :
+    ensure  => 'directory',
+    owner   => $username,
+    group   => $username,
+    mode    => '0755',
+    require => User[$username],
+  }
+
+  create_resources('ssh_authorized_key', $ssh_authorized_keys, {
+    ensure  => 'present',
+    user    => $username,
+    require => User[$username]
+  })
+
+  if($http_ro) {
+    include ::fuel_project::nginx
+
+    if($htpasswd) {
+      $vhost_cfg_append = {
+        auth_basic           => '"Restricted access!"',
+        auth_basic_user_file => "/etc/nginx/${title}.htpasswd",
+      }
+    } else {
+      $vhost_cfg_append = {}
+    }
+    $vhost_cfg_append['disable_symlinks'] = 'if_not_owner'
+
+    if($autoindex) {
+      $vhost_cfg_append['autoindex'] = 'on'
+    } else {
+      $vhost_cfg_append['autoindex'] = 'off'
+    }
+
+    ::nginx::resource::vhost { $title :
+      ensure           => 'present',
+      access_log       => $nginx_access_log,
+      error_log        => $nginx_error_log,
+      format_log       => $nginx_log_format,
+      www_root         => $path,
+      server_name      => $service_fqdn,
+      vhost_cfg_append => $vhost_cfg_append,
+      require          => File[$path]
+    }
+  }
+
+  if($rsync_ro) {
+    include ::rsync::server
+
+    ::rsync::server::module { $title :
+      comment         => $title,
+      uid             => 'nobody',
+      gid             => 'nogroup',
+      list            => 'yes',
+      lock_file       => "/var/run/rsync-${title}.lock",
+      max_connections => 100,
+      path            => $path,
+      read_only       => 'yes',
+      write_only      => 'no',
+      require         => File[$path],
+    }
+  }
+
+  if($rsync_rw) {
+    include ::rsync::server
+
+    ::rsync::server::module{ "${title}-sync" :
+      comment         => $rsync_rw_share_comment,
+      uid             => $username,
+      gid             => $username,
+      hosts_allow     => $sync_hosts_allow,
+      hosts_deny      => ['*'],
+      incoming_chmod  => '0755',
+      outgoing_chmod  => '0644',
+      list            => 'yes',
+      lock_file       => "/var/run/rsync-${title}-sync.lock",
+      max_connections => 100,
+      path            => $path,
+      read_only       => 'no',
+      write_only      => 'no',
+      require         => [
+          File[$path],
+          User[$username],
+        ],
+    }
+  }
+
+  if($htpasswd) {
+    file { "/etc/nginx/${title}.htpasswd" :
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'www-data',
+      mode    => '0440',
+      content => $htpasswd,
+    }
+  }
+}
