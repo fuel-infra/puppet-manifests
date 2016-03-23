5162edb3953c80ebfd6709164946bcaf5fb27d69
Pawel Brzozowski <pbrzozowski@mirantis.com>
2016-03-23 21:06:00 +0100

    fixes for pxetool deployment
    - added required privileges
    - sorted privileges list
    - added 100MB max_body_size limt for Nginx
    - correct smart_proxy package (server instead of client)
    - added syslinux package (pxelinux.0 is required to boot instance)
    - added copy instruction to place pxelinux.0 in tftp directory
    
    Change-Id: Ie264d381e0d1f980769a10b340f72e7e44ada52b

--- modules/pxetool/manifests/smart_proxy.pp
+++ modules/pxetool/manifests/smart_proxy.pp
@@ -1,10 +1,19 @@
 # Class: pxetool::smart_proxy
 #
-# This class installs Trivial File Transfer Protocol Server package.
+# This class installs Trivial File Transfer Protocol Server package, syslinux
+# package and then copies required pxelinux.0 file to tftp directory.
 #
 class pxetool::smart_proxy {
   $packages = [
-    'tftp-hpa',
+    'syslinux-common',
+    'tftpd-hpa',
   ]
+
   ensure_packages($packages)
+
+  file { '/var/lib/tftpboot/pxelinux.0':
+    ensure  => 'present',
+    source  => '/usr/lib/syslinux/pxelinux.0',
+    require => Package[$packages],
+  }
 }
