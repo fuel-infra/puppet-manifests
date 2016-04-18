5de95a14489de1deaac411c6ebb979947c0be4e3
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-04-18 12:12:29 +0200

    Be able to add more ssh keys for jenkins user
    To backup jenkins data on ci-storage.fuel-infra.org it is required
    to created a dedicated ssh key pair and use it when rsyncing the data.
    
    Also added cron_jobs parameter, to be able to specify cron jobs on the
    master.
    
    Related Jira-ticket: https://mirantis.jira.com/browse/PROD-1014
    Change-Id: I7e816de0c4289ccd448610303ce5094baf30b754

--- modules/fuel_project/manifests/jenkins/master.pp
+++ modules/fuel_project/manifests/jenkins/master.pp
@@ -3,28 +3,22 @@
 # This class deploys full Jenkins master instance.
 #
 # Parameters:
+#   [*cron_jobs*] - hash for creating cron jobs
 #   [*firewall_enable*] - enable embedded firewall rules
 #   [*install_label_dumper*] - unused variable
 #   [*install_zabbix_item*] - install Jenkins items for Zabbix
 #   [*jobs_dir*] - path to directory with Jenkins jobs
-#   [*log_gzip_cron_name*] - name of cron entry
-#   [*log_gzip_cron_params*] - cron entry parameters
-#   [*log_gzip_days*] - how many days old files to compress
 #   [*log_gzip_enable*] - enable gzip process for old files
-#   [*log_gzip_threads*] - how many threads to use by pigz
 #   [*service_fqdn*] - service FQDN
 #
 class fuel_project::jenkins::master (
+  $cron_jobs            = undef,
   $firewall_enable      = false,
   $install_label_dumper = false,
   $install_plugins      = false,
   $install_zabbix_item  = false,
   $jobs_dir             = '/var/lib/jenkins/jobs/',
-  $log_gzip_cron_name   = 'jenkins-log-compression',
-  $log_gzip_cron_params = {},
-  $log_gzip_days        = '1',
   $log_gzip_enable      = false,
-  $log_gzip_threads     = '1',
   $service_fqdn         = $::fqdn,
 ) {
   class { '::fuel_project::common':
@@ -38,13 +32,6 @@ class fuel_project::jenkins::master (
   }
   if($log_gzip_enable) {
     ensure_packages('pigz')
-    create_resources('cron', { "${log_gzip_cron_name}" => $log_gzip_cron_params }, {
-        ensure  => 'present',
-        command => "/usr/bin/flock -xn /var/lock/${log_gzip_cron_name}.lock /usr/bin/find ${jobs_dir} -name 'log' -regex '.*/builds/[0-9]+/log' -mtime +${log_gzip_days} -exec pigz -p ${log_gzip_threads} '{}' \; 2>&1 | logger -t jenkins-log-compression",
-        user    => 'jenkins',
-        minute  => '30',
-        hour    => '2',
-    })
   }
 
   # error-pages
@@ -66,6 +53,29 @@ class fuel_project::jenkins::master (
     ssl_only => true,
     www_root => '/usr/share/error_pages',
     require  => Package['error-pages'],
+  }
+
+  $jenkins_additional_ssh_keys = hiera_hash('fuel_project::jenkins::master::jenkins_additional_ssh_keys', {})
+
+  if($jenkins_additional_ssh_keys) {
+    create_resources(file, $jenkins_additional_ssh_keys, {
+      ensure  => 'present',
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      mode    => '0644',
+      require => [
+        User['jenkins'],
+        File["${jenkins_libdir}/.ssh"],
+      ],
+    })
+  }
+
+  if($cron_jobs) {
+    create_resources(cron, $cron_jobs, {
+      ensure  => 'present',
+      user    => 'jenkins',
+      require => User['jenkins'],
+    })
   }
 
 }
