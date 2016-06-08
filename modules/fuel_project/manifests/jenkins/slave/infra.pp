7f0ed0c2f1bf2afd60fea952a8196a94b6cb0819
Alexander Charykov <acharykov@mirantis.com>
2016-06-08 14:33:20 +0300

    Dependencies for run test against racks
    Missing build dependecies for ldap and lauchpadlib libraries:
    * libffi-dev
    * libldap2-dev
    * libsasl2-dev
    * libssl-dev
    
    Change-Id: Iabdcdcc7ecf02aa16b8f3c4875df77de8b559c1d

--- modules/fuel_project/manifests/jenkins/slave/infra.pp
+++ modules/fuel_project/manifests/jenkins/slave/infra.pp
@@ -5,6 +5,10 @@
 class fuel_project::jenkins::slave::infra {
   $packages = [
     'bind9utils',
+    'libffi-dev',
+    'libldap2-dev',
+    'libsasl2-dev',
+    'libssl-dev',
   ]
 
   ensure_packages($packages)
