e65d28dcbc6f5590310508557d1203a0e2714597
Pawel Brzozowski <pbrzozowski@mirantis.com>
2016-09-12 18:36:58 +0200

    updated perestroika::builder class
    - removed non generic known_hosts usage
    - removed docker version declaration
    - removed build_user parameter
    
    Another change to make roles more generic and to fix actual deployment.
    
    Change-Id: I00e0033e755151157e873183daef6ceaf4af823c

--- modules/fuel_project/manifests/roles/perestroika/builder.pp
+++ modules/fuel_project/manifests/roles/perestroika/builder.pp
@@ -3,16 +3,10 @@
 # This class deploys Jenkins slave host for building packages.
 #
 # Parameters:
-#   [*docker_package*] - docker package name
-#   [*builder_user*] - builder user
-#   [*known_hosts*] - known_host file entries
 #   [*packages*] - packages required for builder
 #
 
 class fuel_project::roles::perestroika::builder (
-  $docker_package,
-  $builder_user = 'jenkins',
-  $known_hosts  = undef,
   $packages     = [
     'createrepo',
     'devscripts',
@@ -23,28 +17,7 @@ class fuel_project::roles::perestroika::builder (
   ],
 ){
 
-  # ensure build user exists
-  ensure_resource('user', $builder_user, {
-    'ensure' => 'present'
-  })
-
   # install required packages
   ensure_packages($packages)
-  ensure_packages($docker_package)
-
-  # ensure $builder_user in docker group
-  # docker group will be created by docker package
-  User <| title == $builder_user |> {
-    groups  +> 'docker',
-    require => Package[$docker_package],
-  }
-
-  if ($known_hosts) {
-    create_resources('ssh::known_host', $known_hosts, {
-      user      => $builder_user,
-      overwrite => false,
-      require   => User[$builder_user],
-    })
-  }
 
 }
