2fcc108810654082bfe42747cd9fbe17dbf60eb8
Pawel Brzozowski <pbrzozowski@mirantis.com>
2017-04-21 10:11:10 +0200

    Revert deleting of the authorized keys file
    Because of https://projects.puppetlabs.com/issues/19994
    we are not able to use that functionality yet.
    
    Change-Id: I61aa8b361eab45f5016b7a70464d649d47aacf54

--- modules/ssh/manifests/key_group/atom.pp
+++ modules/ssh/manifests/key_group/atom.pp
@@ -16,18 +16,7 @@ define ssh::key_group::atom (
     $home = "/home/${user}"
   }
 
-  # delete authorized_keys before adding keys
-  if (! defined(File["purge-${user}-authorized_keys"])) {
-    file { "purge-${user}-authorized_keys":
-      path   => "${home}/.ssh/authorized_keys",
-      ensure => 'absent',
-    }
-  }
-
-  ## clear known_hosts file before adding new keys
-  File["purge-${user}-authorized_keys"] -> Ssh_authorized_key <| tag == 'key_group_atomic' |>
-
-  # finally create authorized_key entry
+  # create authorized_key entry
   ssh_authorized_key { $name:
     ensure => present,
     key    => $key,
